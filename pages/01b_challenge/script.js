c04a056f28c7d05fa80a035824bd702841c85ca7
mannubaveja007 <23bis70088@cuchd.in>
2025-11-30 13:14:53 +0530

    01b-Challenge-gsap-installed

---
+++ pages/01b_challenge/script.js
@@ -0,0 +1,9 @@
+gsap.to('.card' , {
+    opacity: 1 ,
+    scale : 1,
+    duration : 6,
+    boxShadow: "2px 2px 5px blue, -2px -2px 5px rgba(81, 141, 237)",
+    yoyo : true,
+});
+
+// rgba(81, 141, 237)
